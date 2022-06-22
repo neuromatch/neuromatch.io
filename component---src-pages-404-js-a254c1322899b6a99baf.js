deb77b6da6f7bc284e1d5dd2553907488fbec955
titipata <titipata@users.noreply.github.com>
2022-06-22 02:12:33 +0000

    deployed via Gatsby Publish Action 🎩 for 3d514172e981c7d23872fc83b880ec6cd747be08

---
+++ component---src-pages-404-js-a254c1322899b6a99baf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{w2l6:function(e,n,t){"use strict";t.r(n);var a=t("Wbzz"),l=t("q1tI"),c=t.n(l),o=t("vOnD"),i=t("Bl7J"),r=o.e.div.withConfig({displayName:"sc-404__Container",componentId:"sc-1envewm-0"})(["text-align:center;"]);n.default=function(){return c.a.createElement(i.a,null,c.a.createElement(r,null,c.a.createElement("h1",null,"Page not found"),c.a.createElement(a.Link,{to:"/"},"Go back")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a254c1322899b6a99baf.js.map
