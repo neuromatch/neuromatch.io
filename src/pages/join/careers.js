dbf9e50d9161add3585bf96544a78b17955411f2
Konstantine Tsafatinos <kosta.tsaf@gmail.com>
2024-02-21 16:21:18 -0500

    update careers page to include new position

--- src/pages/join/careers.js
+++ src/pages/join/careers.js
@@ -36,9 +36,11 @@ const Careers = () => {
       <MainBlock>
         <TitleHeading>Careers</TitleHeading>
         <section>
-          <p>
+            <h3>Research Assistant Professor</h3>
+              <p>We are looking for a part-time or full time Research Assistant Professor to help lead development and evaluation of our global online education program while also establishing our education research program.&nbsp;For more info, see our job posting <a href="https://www.linkedin.com/jobs/view/3835533241/?refId=YG2XYJ9CReadlyw0GQ4lpw%3D%3D&amp;trackingId=YG2XYJ9CReadlyw0GQ4lpw%3D%3D" target="_blank" rel="noreferrer">here</a>.</p>
+{/*          <p>
             There are no open career opportunities at Neuromatch currently.
-          </p>
+          </p> */}
         </section>
       </MainBlock>
       <hr />
