ea2ea3c39ad46776769b72b8c085e2c3dfe1cbc7
Zoltan <kosta.tsaf@gmail.com>
2026-09-15 17:07:28 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,11 @@ module.exports = {
     url: 'http://localhost:8000',
   },
   siteMetadata: {
-    title: 'neuromatch',
+    title: 'neuromatch (archived)',
     description: 'Neuromatch is a 501c3 not-for-profit organization in the United States. We are an online community of computational neuroscientists whose mission is to foster inclusive global interactions for learning, mentorship, networking, and professional development.',
-    siteUrl: 'https://neuromatch.io',
-    image: 'https://neuromatch.io/neuromatch-og-img.png',
-    logo: 'https://neuromatch.io/svgs/logos/Logo_mark_Bl.svg',
+    siteUrl: 'https://old.neuromatch.io',
+    image: 'https://old.neuromatch.io/neuromatch-og-img.png',
+    logo: 'https://old.neuromatch.io/svgs/logos/Logo_mark_Bl.svg',
     type: 'website',
   },
   plugins: [
@@ -25,13 +25,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-153833046-1',
-        exclude: ['/*.png', '/*.ico', '/static/**'],
-      },
-    },
     {
       resolve: 'gatsby-plugin-anchor-links',
       options: {
